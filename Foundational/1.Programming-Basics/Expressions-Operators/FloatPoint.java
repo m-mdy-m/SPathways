5db694563cdb0077e63a5e4ad23eb0be0f78f05a
mahdi <bitsgenix@gmail.com>
2024-10-27 15:56:21 +0330

    feat: Added examples about Operators & Expressions

---
+++ Foundational/1.Programming-Basics/Expressions-Operators/FloatPoint.java
@@ -0,0 +1,12 @@
+class FloatPoint{
+    public static void main(String args[]){
+        float a = 20.5F, b = 6.4f;
+        System.out.println(" a = " + a);
+        System.out.println(" b = " + b);
+        System.out.println(" a+b = " + (a+b));
+        System.out.println(" a-b = " + (a-b));
+        System.out.println(" a*b = " + (a*b));
+        System.out.println(" a/b = " + (a/b));
+        System.out.println(" a%b = " + (a%b)); 
+    }
+}
---
+++ Foundational/1.Programming-Basics/Expressions-Operators/RelationalOperators.java
@@ -0,0 +1,17 @@
+class RelationalOperators
+{
+ public static void main(String args[])
+ {
+ float a = 15.0F, b = 20.75F, c = 15.0F;
+ System.out.println(" a = " + a);
+ System.out.println(" b = " + b);
+ System.out.println(" c = " + c);
+ System.out.println(" a < b is " + (a<b));
+ System.out.println(" a > b is " + (a>b));
+ System.out.println(" a == c is " + (a==c));
+ System.out.println(" a <= c is " + (a<=c));
+ System.out.println(" a >= b is " + (a>=b));
+ System.out.println(" b != c is " + (b!=c));
+ System.out.println(" b == a+c is " + (b==a+c));
+ }
+}
